efec97972c5697778550ba29d13be24d8b1f58ee
NII-John Jacob C. Coluna <nii-jjccoluna@headoffice.corp.bpi.com.ph>
2026-01-09 15:49:40 +0800

    M4_Activity1

--- BankAccount.java
+++ BankAccount.java
@@ -2,35 +2,27 @@ package M4_Activity;
 
 public class BankAccount {
 
-	    public static String getAccountName(String accountNumber) {
-	        if (accountNumber.equals("ACC-001")) 
-	        {
-	            return "Juan Dela Cruz";
-	        } 
-	        else if (accountNumber.equals("ACC-002")) 
-	        {
-	            return "Maria Santos";
-	        } 
-	        else 
-	        {
-	            return null; // Account not found
-	        }
-	    }
+	public static String getAccountName(String accountNumber) {
+		if (accountNumber.equals("ACC-001")) {
+			return "Juan Dela Cruz";
+		} else if (accountNumber.equals("ACC-002")) {
+			return "Maria Santos";
+		} else {
+			return null; // Account not found
+		}
+	}
 
-	    public static void testCase(String accountNumber) {
-	        System.out.println("\nLooking up account: " + accountNumber);
-	        try 
-	        {
-	            
-	        	String name = getAccountName(accountNumber);
-	            String upperName = name.toUpperCase();
-	            System.out.println("Account holder: " + upperName);
-	            
-	        } 
-	        catch (NullPointerException e) 
-	        {
-	            System.out.println("Error: Account not found!");
-	        }
-	    }
+	public static void testCase(String accountNumber) {
+		System.out.println("\nLooking up account: " + accountNumber);
+		try {
+
+			String name = getAccountName(accountNumber);
+			String upperName = name.toUpperCase();
+			System.out.println("Account holder: " + upperName);
+
+		} catch (NullPointerException e) {
+			System.err.println("Error: Account not found!");
+		}
+	}
 
 }
